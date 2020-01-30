1d027f0875a72eb032f3a144dd7cb170642c23b8
Khanh Duy <duy.nguyenkhanh.ncc@gmail.com>
2020-01-31 02:33:34 +0700

    init

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 import { errorRoute } from './layouts/error/error.route';
 import { navbarRoute } from './layouts/navbar/navbar.route';
 import { DEBUG_INFO_ENABLED } from 'app/app.constants';
@@ -8,27 +8,24 @@ import { UserRouteAccessService } from 'app/core/auth/user-route-access-service'
 
 const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
 
+const routes: Route[] = [
+  {
+    path: 'admin',
+    data: {
+      authorities: ['ROLE_ADMIN']
+    },
+    canActivate: [UserRouteAccessService],
+    loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
+  },
+  ...LAYOUT_ROUTES
+];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(
-      [
-        {
-          path: 'admin',
-          data: {
-            authorities: ['ROLE_ADMIN']
-          },
-          canActivate: [UserRouteAccessService],
-          loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)
-        },
-        {
-          path: 'account',
-          loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
-        },
-        ...LAYOUT_ROUTES
-      ],
-      { enableTracing: DEBUG_INFO_ENABLED }
-    )
-  ],
+  imports: [RouterModule.forRoot(routes, { enableTracing: DEBUG_INFO_ENABLED })],
   exports: [RouterModule]
 })
 export class SunAppRoutingModule {}
